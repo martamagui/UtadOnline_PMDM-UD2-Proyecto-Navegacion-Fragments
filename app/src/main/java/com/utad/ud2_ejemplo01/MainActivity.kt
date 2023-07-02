82ad0f404c216c7563d66cb15770a3471efa345d
martamagui <73697174+martamagui@users.noreply.github.com>
2023-07-02 16:57:57 +0200

    Initial commit

---
+++ app/src/main/java/com/utad/ud2_ejemplo01/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.utad.ud2_ejemplo01
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
